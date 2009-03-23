1815f7fc3f321e7e7ff5ed476cbf1d58a14d0b87
Jorge Servin <servin7@gmail.com>
2009-03-22 22:15:56 -0500

    add Encode/DecodeURIComponent() functions

--- api/file_utils.h
+++ api/file_utils.h
@@ -135,6 +135,16 @@ namespace kroll
 		static std::string GetOSVersion();
 
 		/**
+		 * Encodes a URI value
+		 */
+		static std::string FileUtils::EncodeURIComponent(std::string value);
+
+		/**
+		 * Encodes a URI value
+		 */
+		static std::string FileUtils::DecodeURIComponent(std::string value);
+
+		/**
 		 * This function returns temporary directory for application
 		 */
 		static std::string GetTempDirectory();
