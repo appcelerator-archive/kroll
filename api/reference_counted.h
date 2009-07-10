592477fd98fd73c31a3497f67924b82ca76fb6dd
Martin Robinson <mrobinson@appcelerator.com>
2009-07-10 01:42:02 -0700

    Missing file from last commit

---
+++ api/reference_counted.h
@@ -0,0 +1,33 @@
+/**
+ * Appcelerator Kroll - licensed under the Apache Public License 2
+ * see LICENSE in the root folder for details on the license.
+ * Copyright (c) 2009 Appcelerator, Inc. All Rights Reserved.
+ */
+#ifndef _KR_REFERNCE_COUNTED_H_
+#define _KR_REFERNCE_COUNTED_H_
+
+namespace kroll
+{
+	class KROLL_API ReferenceCounted
+	{
+		private:
+		unsigned int referenceCount;
+
+		public:
+		ReferenceCounted() : referenceCount(1) { }
+
+		virtual void duplicate()
+		{
+			referenceCount++;
+		}
+
+		virtual void release()
+		{
+			referenceCount--;
+			if (referenceCount <= 0) {
+				delete this;
+			}
+		}
+	};
+}
+#endif
