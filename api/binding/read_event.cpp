6cfcdb1d7446855bffb56615e0e23d962c08d15d
Martin Robinson <mrobinson@appcelerator.com>
2009-07-16 19:06:50 -0700

    Forgot to add these files

---
+++ api/binding/read_event.cpp
@@ -0,0 +1,22 @@
+/*
+ * Appcelerator Kroll - licensed under the Apache Public License 2
+ * see LICENSE in the root folder for details on the license.
+ * Copyright (c) 2009 Appcelerator, Inc. All Rights Reserved.
+ */
+
+#include "../kroll.h"
+namespace kroll
+{
+	ReadEvent::ReadEvent(AutoPtr<KEventObject> target, AutoBlob data) :
+		Event::Event(target, Event::READ),
+		data(data)
+	{
+		this->SetMethod("getData", &ReadEvent::_GetData);
+	}
+
+	void ReadEvent::_GetData(const ValueList&, SharedValue result)
+	{
+		result->SetObject(this->data);
+	}
+}
+
