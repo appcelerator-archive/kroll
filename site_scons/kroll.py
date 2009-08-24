221ec67174b8ddef3969df52fb10561469757b48
Martin Robinson <mrobinson@appcelerator.com>
2009-08-24 16:44:54 -0700

    Build fixes for PHP on OS X. Cleanup OS X build flags a little.

--- site_scons/kroll.py
+++ site_scons/kroll.py
@@ -211,22 +211,23 @@ class BuildConfig(object):
 
 		if self.is_osx():
 			if ARGUMENTS.get('osx_10_4', 0):
-				OSX_SDK = '/Developer/SDKs/MacOSX10.4u.sdk'
-				OSX_MINVERS = '-mmacosx-version-min=10.4'
+				sdk_dir = '/Developer/SDKs/MacOSX10.4u.sdk'
+				sdk_minversion = '-mmacosx-version-min=10.4'
 				self.env['GCC_VERSION'] = '4.0'
 				self.env['MACOSX_DEPLOYMENT_TARGET'] = '10.4'
 			else:
-				OSX_SDK = '/Developer/SDKs/MacOSX10.5.sdk'
-				OSX_MINVERS = '-mmacosx-version-min=10.5'
+				sdk_dir = '/Developer/SDKs/MacOSX10.5.sdk'
+				sdk_minversion = '-mmacosx-version-min=10.5'
 				self.env['MACOSX_DEPLOYMENT_TARGET'] = '10.5'
 
-			OSX_UNIV_LINKER = '-isysroot '+OSX_SDK+' -syslibroot,'+OSX_SDK+' -arch i386 -arch ppc -lstdc++ ' + OSX_MINVERS 
-			self.env.Append(CXXFLAGS=['-isysroot',OSX_SDK,'-arch','i386',OSX_MINVERS,'-x','objective-c++'])
-			self.env.Append(CPPFLAGS=['-arch','i386'])
-			self.env.Append(CPPFLAGS=['-arch','ppc'])
-			self.env.Append(LINKFLAGS=OSX_UNIV_LINKER)
+			self.env['CC'] = ['gcc', '-arch', 'i386', '-arch', 'ppc']
+			self.env['CXX'] = ['gcc', '-arch', 'i386', '-arch', 'ppc']
 			self.env.Append(FRAMEWORKS=['Foundation'])
-			self.env.Append(CPPFLAGS=['-Wall', '-fno-common','-fvisibility=hidden','-DMACOSX_DEPLOYMENT_TARGET='+self.env['MACOSX_DEPLOYMENT_TARGET']])
+			self.env.Append(CXXFLAGS=['-isysroot', sdk_dir, sdk_minversion, '-x', 'objective-c++'])
+			self.env.Append(LINKFLAGS=['-isysroot', sdk_dir, '-syslibroot,' + sdk_dir, '-lstdc++', sdk_minversion])
+			self.env.Append(CPPFLAGS=[
+				'-Wall', '-fno-common', '-fvisibility=hidden',
+				'-DMACOSX_DEPLOYMENT_TARGET=' + self.env['MACOSX_DEPLOYMENT_TARGET']])
 
 	def matches(self, n): return bool(re.match(os.uname()[0], n))
 	def is_linux(self): return self.os == 'linux'
